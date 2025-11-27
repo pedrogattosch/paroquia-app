b7aa8d255137df71822110c93691049eb835c020
Pedro Miguel Gatto Schardong <pedrogattosch@gmail.com>
2025-11-27 17:57:01 -0300

    feat: add new colors in colors.dart

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -5,7 +5,11 @@ const Color kPrimaryColor = Color(0xFF1565C0);
 const Color kSecondaryColor = Color(0xFF42A5F5);
 const Color kBackgroundColor = Color(0xFFF5F5F5);
 const Color kTextColor = Color(0xFF212121);
+const Color kTitleColor = Color(0xFF1C4587);
+const Color kCardTitleColor = Color(0xFF37474F);
+const Color kCardSubtitleColor = Color(0xFF546E7A);
 const Color kBorderColor = Color(0xFFBDBDBD);
+const Color kSuccessColor = Color(0xFF4CAF50);
 
 const LinearGradient kAppBarGradient = LinearGradient(
   colors: [kPrimaryColor, kSecondaryColor],
